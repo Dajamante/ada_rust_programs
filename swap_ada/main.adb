f70703fe73f135d8d3113fc0c1f3a3545b998f08
aissata <aimaiga2@gmail.com>
2023-01-12 15:03:50 +0000

    Florian comments

--- swap_ada/main.adb
+++ swap_ada/main.adb
@@ -3,6 +3,7 @@ with Ada.Text_IO;
 procedure Main is
 
    procedure Swap (X, Y : in out Integer) with
+   -- Convention == Extern in Rust
      Import, Convention => C, Global => null,
      Annotate           => (GNATprove, Always_Return),
      Post               => X = Y'Old and then Y = X'Old;
@@ -11,6 +12,7 @@ procedure Main is
    Y : Integer := 2;
 begin
    Ada.Text_IO.Put_Line ("X =" & X'Img & "; Y =" & Y'Img);
+   -- There is an external function call Swap with the C convention
    Swap (X, Y);
    pragma Assert (X = 2 and Y = 1);
    Ada.Text_IO.Put_Line ("X =" & X'Img & "; Y =" & Y'Img);
