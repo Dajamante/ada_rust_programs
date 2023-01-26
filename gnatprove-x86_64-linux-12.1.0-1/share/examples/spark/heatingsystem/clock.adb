7a4ba5aa5c26e9135e48d57ccf0e089566097720
aissata <aimaiga2@gmail.com>
2023-01-26 16:23:41 +0100

    added an executable of gnatprove as I needed it

---
+++ gnatprove-x86_64-linux-12.1.0-1/share/examples/spark/heatingsystem/clock.adb
@@ -0,0 +1,21 @@
+with System.Storage_Elements;
+
+-- Clock
+package body Clock
+  with Refined_State => (Ticks => Tick_Ext)
+is
+   Tick_Ext : Times
+     with Volatile,
+          Async_Writers,
+          Address => System.Storage_Elements.To_Address (16#FFFF_FFFF#);
+   pragma Annotate(Gnatprove, Intentional, "aliasing via address clause","");
+
+   procedure Read (Time : out Times)
+     with Refined_Global  => Tick_Ext,
+          Refined_Depends => (Time => Tick_Ext)
+   is
+   begin
+      Time := Tick_Ext;
+   end Read;
+
+end Clock;
---
+++ gnatprove-x86_64-linux-12.1.0-1/share/examples/spark/heatingsystem/display.adb
@@ -0,0 +1,21 @@
+with System.Storage_Elements;
+
+-- Control Panel Boundary Packages
+package body Display
+  with Refined_State => (Outputs => Output_Ext)
+is
+   Output_Ext : Displays
+     with Volatile,
+          Async_Readers,
+          Address => System.Storage_Elements.To_Address (16#FFFF_FFFF#);
+   pragma Annotate(Gnatprove, Intentional, "aliasing via address clause","");
+
+   procedure Write (Content : in Displays)
+     with Refined_Global  => (Output => Output_Ext),
+          Refined_Depends => (Output_Ext => Content)
+   is
+   begin
+      Output_Ext := Content;
+   end Write;
+
+end Display;
