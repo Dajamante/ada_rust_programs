4626c86bc3fd7c3f25694f6a6567f20393fc7fd3
aissata <aimaiga2@gmail.com>
2023-01-30 10:35:55 +0000

    disassembled code for enums

--- print_enum/src/main.adb
+++ print_enum/src/main.adb
@@ -1,12 +1,9 @@
-with Ada.Text_IO; use Ada.Text_IO;
-
 procedure Main is
-    type TwoNum is (One, Two) with
+    type TwoNum is (Enum11One, Enum22Two) with
        Size => 8;
     procedure Print_enum (one : TwoNum; two : TwoNum) with
        Import, Convention => C, Annotate => Terminating, Global => null;
 
 begin
-    Put_Line ("In Ada, " & One'Img & " and " & Two'Img);
-    Print_enum (One, Two);
+    Print_enum (Enum11One, Enum22Two);
 end Main;
