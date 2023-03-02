3332bf81b49ebb4917fcb62c08676618c7f9e78b
aissata <aimaiga2@gmail.com>
2023-03-02 14:24:24 +0000

    solved with standalone lib

--- Rust_to_Ada/ada_adder/adder/src/adder.ads
+++ Rust_to_Ada/ada_adder/adder/src/adder.ads
@@ -1,6 +1,9 @@
 package Adder is
 
-    function Add (X, Y: in Integer) return Integer with
-       Export, Convention => C, SPARK_mode => On, External_Name => "add";
+   function Add (X, Y: in Integer) return Integer with
+      Export, Convention => C, SPARK_mode => On,
+      Annotate => (GNATprove, Terminating),
+      Post => (Add'Result = X + Y),
+      External_Name => "add";
 
 end Adder;
