ef1dd36d8acdfe03973732555a90b9e29d0e686e
aissata <aimaiga2@gmail.com>
2023-03-03 09:43:35 +0000

    experiments with graphics

--- Rust_to_Ada/ada_adder/src/main.rs
+++ Rust_to_Ada/ada_adder/src/main.rs
@@ -1,21 +1,13 @@
 extern "C" {
-    fn add(x: Box<i32>, y: Box<i32>) -> i32;
-}
-
-#[macro_export]
-macro_rules! to_ada {
-    ($x:expr, $y:expr) => {{
-        let a = $x as i32;
-        let b = $y as i32;
-        let mut z = 0;
-        unsafe { z = add(a,b) };
-        z
-    }};
+    fn add(x: *mut i32, y: *mut i32) -> i32;
 }
 
 
 fn main() {
     let x = Box::new(1);
-    let y = Box::new(2);
-    println!("The unsafe z is {:?}", unsafe { add(x,y) });
+    let y = Box::new(6);
+    let x_ptr = Box::into_raw(x);
+    let y_ptr = Box::into_raw(y);
+    println!("The unsafe z is {:?}", unsafe { add(x_ptr,y_ptr) });
+    
 }
