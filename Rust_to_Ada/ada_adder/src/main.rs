8419c6be0259abe85feecdf5153d04bb1ccd6d5e
aissata <aimaiga2@gmail.com>
2023-03-03 10:39:09 +0000

    simplest example

--- Rust_to_Ada/ada_adder/src/main.rs
+++ Rust_to_Ada/ada_adder/src/main.rs
@@ -1,13 +1,10 @@
 extern "C" {
-    fn add(x: *mut i32, y: *mut i32) -> i32;
+    fn add(x: &i32, y: &i32) -> i32;
 }
 
 
 fn main() {
-    let x = Box::new(1);
-    let y = Box::new(6);
-    let x_ptr = Box::into_raw(x);
-    let y_ptr = Box::into_raw(y);
-    println!("The unsafe z is {:?}", unsafe { add(x_ptr,y_ptr) });
-    
+    let x = 1;
+    let y = 2;
+    let z = unsafe { add(&x,&y) };
 }
