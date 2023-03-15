9b135c9f628e8cce3049a59bfc9e03ee3192e035
aissata <aimaiga2@gmail.com>
2023-03-15 13:02:12 +0000

    correct pointer to structure with different alignments

--- Ada_to_Rust/composite_pointer/main.adb
+++ Ada_to_Rust/composite_pointer/main.adb
@@ -4,11 +4,12 @@ procedure Main with
    SPARK_Mode
 is
     type Pointed is record
-        X : Integer         := 0;
+        X : Integer         := 42;
         S : String (1 .. 5) := "Hello";
     end record;
     procedure Overwrite (Ptr : access Pointed) with
        Import, Convention => C, Global => null,
+       -- Why is the post condition not triggered
        Post               => Ptr.X = 16 and Ptr.S = "Bluey";
 
     -- pointer.adb:9:13: error: unconstrained subtype in component declaration
@@ -16,22 +17,20 @@ is
     type PtrPointed is access all Pointed;
 
     -- P needs to be declared as an aliased object.
-    P   : aliased Pointed := (42, "World");
+    P   : aliased Pointed;
     -- Pointing to a new record created using the values of P instead.
     -- Ptr : PtrPointed      := new Pointed'(P);
-    Ptr : PtrPointed      := P'Access;
+    Ptr : PtrPointed := P'Access;
 
     procedure Print_out (Ptr : access Pointed) is
     begin
-        Put_Line ("P is " & P'Img);
-        Put_Line ("P alignment is " & P'Alignment'Img);
-        Put_Line ("P size is " & P'Size'Img);
+        --Put_Line ("P is " & P'Img);
+        --Put_Line ("P alignment is " & P'Alignment'Img);
+        --Put_Line ("P size is " & P'Size'Img);
         Put_Line ("Ptr is " & Ptr.all'Img);
     end Print_out;
 
 begin
-    P.X := 19;
-    P.S := "World";
     -- This is what needs to be done in Rust
     -- Ptr.X := 16;
     -- Ptr.S := "Bluey";
--- Ada_to_Rust/composite_pointer/pointer.rs
+++ Ada_to_Rust/composite_pointer/pointer.rs
@@ -1,15 +1,24 @@
+use std::convert::TryInto;
+
+// #[repr(C)]
+// pub struct AdaString {
+//     length: u32,
+//     data: [u8; 5], // assume max length of 5
+// }
 #[repr(C)]
-pub struct Pointed<'a> {
+pub struct Pointed {
     x: i32,
     // we will give away ownership
     // does it need to be a bounded string?
-    s: &'a str,
+    s: [u8; 5],
 }
 
+// This is a way to find for the symbol
+//  nm -gC --defined-only libpointer.a > log.log
 #[no_mangle]
 pub extern "C" fn overwrite(ptr: &mut Pointed) {
     ptr.x = 16;
-    ptr.s = "Bluey";
-
+    ptr.s = "Bluey".as_bytes().try_into().expect("Could not convert.");
+    // ptr.s.data will yield incorrect result.
     // do we need to forget ownership here?
 }
