9dd925ac9fb5d2c602fe511223348f814098ccde
aissata <aimaiga2@gmail.com>
2023-04-11 16:21:32 +0000

    compiling

---
+++ bbqueue/rust/bbq/printer/src/printer.adb
@@ -0,0 +1,19 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+with System.Storage_Elements; use System.Storage_Elements;
+with BBqueue;
+with BBqueue.Buffers;
+with System;                  use System;
+
+package body Printer is
+    procedure Fill (WG : BBqueue.Buffers.Write_Grant; Val : Storage_Element) is
+        pragma SPARK_Mode (Off);
+
+        S   : constant BBqueue.Buffers.Slice_Rec := BBqueue.Buffers.Slice (WG);
+        Arr : Storage_Array (1 .. S.Length) with
+           Address => S.Addr;
+    begin
+        Put_Line ("Fill" & S.Length'Img & " bytes.");
+        Arr := (others => Val);
+    end Fill;
+end Printer;
---
+++ bbqueue/rust/bbq/printer/src/printer.ads
@@ -0,0 +1,4 @@
+package Printer is
+    pragma Elaborate_Body;
+
+end Printer;
