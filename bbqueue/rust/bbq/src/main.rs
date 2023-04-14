ec1db859dbaee775b7664abc0cd04533e8d6ee20
aissata <aimaiga2@gmail.com>
2023-04-14 14:21:48 +0000

    changed bbqueue to ffi compatible

--- bbqueue/rust/bbq/src/main.rs
+++ bbqueue/rust/bbq/src/main.rs
@@ -1,9 +1,18 @@
+mod bbq_ipc;
+use bbq_ipc::*;
+// LIBRARY_TYPE=relocatable alr build
+// eval $(alr -q printenv --unix)
+// LD_ ...
 extern "C" {
-    fn fill();
+    fn fill(w: GrantW, s: i32);
 }
 fn main() {
-    println!("Hello, world!");
+    //let mut bb = BBBuffer::new();
+    //bb.initialize(0, 4);
+    //let mut prod = unsafe { bbbuffer::BBBuffer::take_producer(&mut bb) };
+    //let mut grant = prod.grant_exact(1).unwrap();
+
     unsafe {
-        fill();
+        fill(grant, 4);
     }
 }
