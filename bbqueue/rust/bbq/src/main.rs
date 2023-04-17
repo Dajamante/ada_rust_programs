1937ed282c082de2de10b82960635afe1a564cf4
aissata <aimaiga2@gmail.com>
2023-04-17 07:03:12 +0000

    maybe heap allocated

--- bbqueue/rust/bbq/src/main.rs
+++ bbqueue/rust/bbq/src/main.rs
@@ -1,9 +1,9 @@
 use core::cell::UnsafeCell;
 mod bbq_ipc;
 use bbq_ipc::*;
-// LIBRARY_TYPE=relocatable alr build
-// eval $(alr -q printenv --unix)
-// LD_LIBRARY_PATH=printer/lib cargo run
+// in printer/: LIBRARY_TYPE=relocatable alr build
+// still in printer/: eval $(alr -q printenv --unix)
+// outside printer: LD_LIBRARY_PATH=printer/lib cargo run
 extern "C" {
     fn fill();
 }
@@ -12,25 +12,31 @@ extern "C" {
 // as long as both A and B exist
 
 fn main() {
-    let bbq: BBBuffer = BBBuffer::new();
-    println!("BBBuffer bbq {:?}", bbq);
-    let data: UnsafeCell<[u8; 1024]> = UnsafeCell::new([0u8; 1024]);
-    println!("data {:?}", data);
+    //let bbq: BBBuffer = BBBuffer::new();
+    let bbq: Box<BBBuffer> = Box::new(BBBuffer::new());
+    // let bbq: Box<BBBuffer> = Box::new(BBBuffer::new());
+
+    println!("bbq {:?}\n", bbq);
+    // let data: UnsafeCell<[u8; 1024]> = UnsafeCell::new([0u8; 1024]);
+    let data: Box<UnsafeCell<[u8; 1024]>> = Box::new(UnsafeCell::new([0u8; 1024]));
+    println!("Inner data {:?}\n", data);
     // Ok this is gross but I guess I need it?
     // how to get that heap allocated
-    let buf_ptr = (&bbq as *const BBBuffer).cast_mut();
-
-    println!("buf_ptr {:?}", buf_ptr);
+    //let buf_ptr = (&bbq as *const BBBuffer).cast_mut();
+    // let buf_ptr = &bbq as *const BBBuffer as *mut BBBuffer;
+    let buf_ptr = Box::into_raw(bbq);
+    println!("buf_ptr: {:?}\n", buf_ptr);
     let producer = unsafe {
         // initialize ONCE
+        // Signature:
         //fn initialize(&'a self, buf_start: *mut u8, buf_len: usize)
-        bbq.initialize(data.get().cast::<u8>(), 1024);
+        (*buf_ptr).initialize(data.as_ref().get().cast::<u8>(), 1024);
         BBBuffer::take_producer(buf_ptr)
     };
 
     let grant = producer.grant_exact(4).unwrap();
-    println!("producer {:?}", producer);
-    println!("grant {:?}", grant);
+    println!("{:?} \n", producer);
+    println!("{:?} \n", grant);
     unsafe {
         fill();
     }
