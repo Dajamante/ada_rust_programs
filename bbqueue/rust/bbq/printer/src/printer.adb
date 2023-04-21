f8b82a8b5927bd27e6f45acc4a2f4498ccf06e22
aissata <aimaiga2@gmail.com>
2023-04-21 12:06:14 +0000

    trying to casting a rust header in a spark header

--- bbqueue/rust/bbq/printer/src/printer.adb
+++ bbqueue/rust/bbq/printer/src/printer.adb
@@ -7,76 +7,69 @@ with System;      use System;
 with Ada.Unchecked_Conversion;
 
 package body Printer is
-   --function Convert_RustBBQ_To_Offsets_Only
-   --  (Rust_BBQ : BBBuffer) return BBqueue.Offsets_Only
-   --is
-   --   Result : BBqueue.Offsets_Only (Size => 0);
-   --begin
-   --  Result.Write              := Atomic_Count.Init (Rust_BBQ.Write);
-   --   Result.Read               := Atomic_Count.Init (Rust_BBQ.Read);
-   --   Result.Last               := Atomic_Count.Init (Rust_BBQ.Last);
-   --   Result.Reserve            := Atomic_Count.Init (Rust_BBQ.Reserve);
-   --   Result.Read_In_Progress   := Atomic.Init (Rust_BBQ.Read_In_Progress);
-   --   Result.Write_In_Progress  := Atomic.Init (Rust_BBQ.Write_In_Progress);
-   --   Result.Granted_Write_Size := 0;
-   --   Result.Granted_Read_Size  := 0;
-   --   return Result;
-   --end Convert_RustBBQ_To_Offsets_Only;
 
-   function Get_BBQ_Data (BBQ : System.Address) return RustBBQstruct with
-     Import, Convention => C, External_Name => "send_bbq_data";
+  --function Convert_RustBBQ_To_Offsets_Only
+  -- (Rust_BBQ : BBBuffer) return access BBqueue.Offsets_Only
+  --is
+  --  Result : BBqueue.Offsets_Only (Size => 128);
+  --begin
+  --  Result.Write              := Atomic_Count.Init (Rust_BBQ.Write);
+  --  Result.Read               := Atomic_Count.Init (Rust_BBQ.Read);
+  --  Result.Last               := Atomic_Count.Init (Rust_BBQ.Last);
+  --  Result.Reserve            := Atomic_Count.Init (Rust_BBQ.Reserve);
+  --  Result.Read_In_Progress   := Atomic.Init (Rust_BBQ.Read_In_Progress);
+  --  Result.Write_In_Progress  := Atomic.Init (Rust_BBQ.Write_In_Progress);
+  --  Result.Granted_Write_Size := 0;
+  --  Result.Granted_Read_Size  := 0;
+  --  return Result;
 
-   procedure Read_queue (RRG : in out RustReadGrant) is
-      type Arr is array (Positive range <>) of Interfaces.Unsigned_8;
-      A : Arr (1 .. 4) with
-        Address => RRG.inner_buf;
+  --end Convert_RustBBQ_To_Offsets_Only;
 
-      type BBBuffer_ptr is access all BBBuffer;
+  function Get_BBQ_Data (BBQ : System.Address) return BBqueue.BBBuffer with
+   Import, Convention => C, External_Name => "send_bbq_data";
 
-      -- copying byte for byte
-      function To_BBBuffer_ptr is new Ada.Unchecked_Conversion
-        (System.Address, BBBuffer_ptr);
-      BBBPtr : BBBuffer_ptr := To_BBBuffer_ptr (RRG.bbq);
+  procedure Read_queue (RRG : in out RustReadGrant) is
+    type Arr is array (Positive range <>) of Interfaces.Unsigned_8;
+    A : Arr (1 .. 4) with
+     Address => RRG.inner_buf;
 
-      --Mirrored_bbq     : aliased BBqueue.Offsets_Only (128);
-      RustOffsets_only : BBBuffer with
-        Address => RRG.bbq'Address;
-        --Mirrored_bbq     : BBqueue.Offsets_Only :=
-        --  Convert_RustBBQ_To_Offsets_Only (RustOffsets_only);
-   begin
+    RustOffsets_only : BBqueue.BBBuffer with
+     Address => RRG.bbq'Address;
+    Mirrored_bbq     : access BBqueue.Offsets_Only :=
+     BBqueue.Convert_RustBBQ (RustOffsets_only);
+  begin
 
-      Put_Line ("SPARK function.");
-      Put_Line
-        ("System.Address_Image (RRG.all'Address):" &
-         System.Address_Image (RRG'Address));
-      Put_Line ("Pointer RRG: " & System.Address_Image (RRG'Address));
-      Put_Line
-        ("System.Address_Image (RRG.bbq'Address), first field of ReadGrant: " &
-         System.Address_Image (RRG.bbq'Address));
+    Put_Line ("SPARK function.");
+    Put_Line
+     ("System.Address_Image (RRG.all'Address):" &
+      System.Address_Image (RRG'Address));
+    Put_Line ("Pointer RRG: " & System.Address_Image (RRG'Address));
+    Put_Line
+     ("System.Address_Image (RRG.bbq'Address), first field of ReadGrant: " &
+      System.Address_Image (RRG.bbq'Address));
 
-      Put_Line (" Rust offsets_only" & RustOffsets_only'Img);
+    Put_Line (" Rust offsets_only" & RustOffsets_only'Img);
 
-      Put_Line
-        ("System.Address_Image (RRG.bbq), the pointer to BBQ " &
-         System.Address_Image (RRG.bbq));
-      Put_Line
-        ("System.Address_Image (RRG.inner_buf'Address)" &
-         System.Address_Image (RRG.inner_buf'Address));
+    Put_Line
+     ("System.Address_Image (RRG.bbq), the pointer to BBQ " &
+      System.Address_Image (RRG.bbq));
+    Put_Line
+     ("System.Address_Image (RRG.inner_buf'Address)" &
+      System.Address_Image (RRG.inner_buf'Address));
 
-      Put_Line
-        ("System.Address_Image (RRG.buf_len'Address)" &
-         System.Address_Image (RRG.buf_len'Address));
-      Put_Line ("RRG.buf_len'Img" & RRG.buf_len'Img);
-      Put_Line
-        ("The pointer to BBQ: " & System.Address_Image (RRG.bbq'Address));
-      Put_Line
-        ("The pointer to Inner Buff: " & System.Address_Image (RRG.inner_buf));
-      Put_Line ("RRG.buf_len: " & RRG.buf_len'Img);
+    Put_Line
+     ("System.Address_Image (RRG.buf_len'Address)" &
+      System.Address_Image (RRG.buf_len'Address));
+    Put_Line ("RRG.buf_len'Img" & RRG.buf_len'Img);
+    Put_Line ("The pointer to BBQ: " & System.Address_Image (RRG.bbq'Address));
+    Put_Line
+     ("The pointer to Inner Buff: " & System.Address_Image (RRG.inner_buf));
+    Put_Line ("RRG.buf_len: " & RRG.buf_len'Img);
 
-      for I in 1 .. 4 loop
-         Put (A (I)'Img);
-      end loop;
-      New_Line;
+    for I in 1 .. 4 loop
+      Put (A (I)'Img);
+    end loop;
+    New_Line;
 
-   end Read_queue;
+  end Read_queue;
 end Printer;
--- bbqueue/rust/bbq/printer/src/printer.ads
+++ bbqueue/rust/bbq/printer/src/printer.ads
@@ -18,36 +18,27 @@ package Printer is
     end record;
     pragma Convention (Ada_Pass_By_Reference, RustReadGrant);
 
-    --   type Offsets_Only (Size : Buffer_Size) is limited record
-    -- Write : aliased Atomic_Count.Instance ;
-    -- Read  : aliased Atomic_Count.Instance ;
-    -- Last  : aliased Atomic_Count.Instance ;
-    -- Reserve  : aliased Atomic_Count.Instance;
-    -- Read_In_Progress : aliased Atomic.Flag;
-    -- Write_In_Progress : aliased Atomic.Flag;
-    --Granted_Write_Size : Count := 0;
-    --Granted_Read_Size : Count := 0;
-    type BBBuffer is record
-        Buf               : System.Address; -- not needed
-        Inner_buf         : System.Address; -- not needed
-        Write             : Interfaces.Unsigned_64;
-        Read              : Interfaces.Unsigned_64;
-        Last              : Interfaces.Unsigned_64;
-        Reserve           : Interfaces.Unsigned_64;
-        Read_In_Progress  : Boolean;
-        Write_In_Progress : Boolean;
-        --Granted_Write_Size : Count := 0; missing!
-        --Granted_Read_Size : Count := 0; missing!
-    end record;
-
-    type RustBBQstruct is record
-        Write             : Interfaces.Unsigned_64;
-        Read              : Interfaces.Unsigned_64;
-        Last              : Interfaces.Unsigned_64;
-        Reserve           : Interfaces.Unsigned_64;
-        Read_In_Progress  : Boolean;
-        Write_In_Progress : Boolean;
-    end record;
+    -- type Offsets_Only (Size : Buffer_Size) is limited record
+    --  Write : aliased Atomic_Count.Instance ;
+    --  Read  : aliased Atomic_Count.Instance ;
+    --  Last  : aliased Atomic_Count.Instance ;
+    --  Reserve  : aliased Atomic_Count.Instance;
+    --  Read_In_Progress : aliased Atomic.Flag;
+    --  Write_In_Progress : aliased Atomic.Flag;
+    --  Granted_Write_Size : Count := 0;
+    --  Granted_Read_Size : Count := 0;
+    --type BBBuffer is record
+    --    Buf               : System.Address; -- not needed
+    --    Inner_buf         : System.Address; -- not needed
+    --    Write             : Interfaces.Unsigned_64;
+    --    Read              : Interfaces.Unsigned_64;
+    --    Last              : Interfaces.Unsigned_64;
+    --    Reserve           : Interfaces.Unsigned_64;
+    --    Read_In_Progress  : Boolean;
+    --    Write_In_Progress : Boolean;
+    --Granted_Write_Size : Count := 0; missing!
+    --Granted_Read_Size : Count := 0; missing!
+    --end record;
 
     procedure Read_queue (RRG : in out RustReadGrant) with
        Export, External_Name => "read_queue", SPARK_Mode => On;
