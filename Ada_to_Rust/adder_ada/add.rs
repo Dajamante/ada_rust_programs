1d3c19bcaa80043f7bc263c697c06bce62d66087
aissata <aimaiga2@gmail.com>
2023-05-08 14:22:54 +0000

    Adding integers by copy

--- Ada_to_Rust/adder_ada/add.rs
+++ Ada_to_Rust/adder_ada/add.rs
@@ -3,6 +3,8 @@
 // Handle the error apropriately to not allow undefined behavious
 // at the FFI border.
 #[no_mangle]
-pub extern "C" fn add(x: &mut i32, y: &mut i32, z: &mut i32) {
-    *z = *x + *y;
+//pub extern "C" fn add(x: &mut i32, y: &mut i32, z: &mut i32) {
+pub extern "C" fn add(x: i32, y: i32) -> i32 {
+    //*z = *x + *y + *y;
+    x + y
 }
--- Ada_to_Rust/adder_ada/main.adb
+++ Ada_to_Rust/adder_ada/main.adb
@@ -4,20 +4,20 @@
 with Ada.Text_IO;
 
 procedure Main with
-  SPARK_Mode
+ SPARK_Mode => On
 is
 
-   procedure Add (X, Y, Z : in out Integer) with
-     -- Convention means "Extern" in Rust
-     Import, Convention => C, Global => null,
-     Annotate           => (GNATprove, Terminating), Post => Z = X'Old + Y'Old;
+  function Add (X, Y : Integer) return Integer with
+   -- Convention means "Extern" in Rust
+   Import, Convention => C, Global => null,
+   Annotate           => (GNATprove, Terminating), Post => Z = X'Old + Y'Old;
 
-   X : Integer := 1;
-   Y : Integer := 2;
-   Z : Integer := 0;
+  X : Integer := 1;
+  Y : Integer := 2;
+  Z : Integer := 0;
 begin
-   Ada.Text_IO.Put_Line ("Z =" & Z'Img);
-   Add (X, Y, Z);
-   pragma Assert (Z = 3);
-   Ada.Text_IO.Put_Line ("Z =" & Z'Img);
+  Ada.Text_IO.Put_Line ("Z =" & Z'Img);
+  Z := Add (X, Y);
+  pragma Assert (Z = 5); -- the pragma asserts are useless
+  Ada.Text_IO.Put_Line ("Z =" & Z'Img);
 end Main;
